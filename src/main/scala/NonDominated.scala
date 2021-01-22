81f3b2a2c47b7a846006db92201bb3c599dfb535
Enkhai <aerogtx@gmail.com>
2021-01-22 06:05:38 +0200

    Implemented angular partitioning

--- src/main/scala/NonDominated.scala
+++ src/main/scala/NonDominated.scala
@@ -1,3 +1,5 @@
+import Util.AngularPartitioner
+import Util.Misc.{fillEmpty, getMinValues}
 import Util.SFSSkyline.addScoreAndCalculate
 import org.apache.log4j._
 import org.apache.spark.{SparkConf, SparkContext}
@@ -15,12 +17,29 @@ object NonDominated {
     val sc = new SparkContext(sparkConf)
 
     val currentDir = System.getProperty("user.dir")
-    val inputFile = "file://" + currentDir + "/datasets/gaussian_size1000_dim2.csv"
+    val inputFile = "file://" + currentDir + "/datasets/gaussian_size1000000_dim5.csv"
     val outputDir = "file://" + currentDir + "/output"
 
-    val points = sc.textFile(inputFile)
+    var points = sc.textFile(inputFile)
       .map(x => x.split(","))
       .map(x => x.map(y => y.toDouble))
+
+    // calculate min dimension values for angular partitioning
+    val minVal = points.mapPartitions(getMinValues)
+      .coalesce(1)
+      .mapPartitions(getMinValues)
+      .collect
+      .head
+    // number of partitions should be multiple of 3
+    val numPartitions = 6
+    val partitioner = new AngularPartitioner(numPartitions, minVal)
+
+    points = points
+      .mapPartitions(_.toList.map(x => (partitioner.makeKey(x), x)).toIterator)
+      .partitionBy(partitioner)
+      .mapPartitions(_.toList.map(_._2).toIterator)
+      // fill in empty partitions with dummy data to avoid ArrayOutOfIndex error
+      .mapPartitions(partition => fillEmpty(partition, minVal.length))
 
     val result = points.mapPartitions(addScoreAndCalculate) // calculate the skyline for each partition
       .coalesce(1) // reduce to 1 partition
--- src/main/scala/Util/Misc.scala
+++ src/main/scala/Util/Misc.scala
@@ -1,5 +1,7 @@
 package Util
 
+import scala.collection.mutable.ArrayBuffer
+
 object Misc {
 
   def sortDominationPartition(partition: Iterator[(Array[Double], Int)]): Iterator[(Array[Double], Int)] = {
@@ -8,6 +10,20 @@ object Misc {
 
   def filterDominationPartition(partition: Iterator[(Array[Double], Int)], k: Int = 20): Iterator[Array[Double]] = {
     partition.toList.zipWithIndex.filter(_._2 < k).map(_._1._1).toIterator
+  }
+
+  def getMinValues(partition: Iterator[Array[Double]]): Iterator[Array[Double]] = {
+    val partitionList = partition.toArray.transpose
+    val minValues = ArrayBuffer[Double]()
+    for (i <- partitionList.indices)
+      minValues += partitionList(i).min
+    Iterator(minValues.toArray)
+  }
+
+  def fillEmpty(partition: Iterator[Array[Double]], dim: Int): Iterator[Array[Double]] = {
+    if (partition.isEmpty)
+      return Iterator(Array.fill(dim)(Double.PositiveInfinity))
+    partition
   }
 
 }
